2f31ac61e8652ce26c3f74ce393b8057b66f80c0
romanmmr <romanmmr@yahoo.com>
2022-12-23 13:50:30 +0100

    First steps on creating directories for training, validation and testing

--- directories.py
+++ directories.py
@@ -7,57 +7,95 @@ import shutil
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# All images of the faces are here:
 faces_path = 'D:\Data Science\Projects\Homer_detector\Faces'
 
+# Homer directory:
+homer_dir = os.path.join(faces_path, 'Homer')
+
+# No_Homer directory:
+no_homer_dir = os.path.join(faces_path, 'No_Homer')
+
+# This is the base directory:
 base_dir = 'D:\Data Science\Projects\Homer_detector'
 
-train_dir = os.path.join(base_dir, 'train')
-validation_dir = os.path.join(base_dir, 'validation')
+# Pycharm project directory:
+project_dir = 'C:\\Users\\RodrigoRoman\\PycharmProjects\\Homer_Face_Detector\\Images'
 
+# Training and validation directories will be in the Pycharm project directory
+train_dir = os.path.join(project_dir, 'train')
+validation_dir = os.path.join(project_dir, 'validation')
+test_dir = os.path.join(project_dir, 'test')
+
+# Training and validation directories for images with and without target (homer's face)
 train_homer_dir = os.path.join(train_dir, 'homer')
 train_no_homer_dir = os.path.join(train_dir, 'no_homer')
 
 validation_homer_dir = os.path.join(validation_dir, 'homer')
 validation_no_homer_dir = os.path.join(validation_dir, 'no_homer')
 
-train_percent = 0.80
-train_size = int(len(os.listdir(os.path.join(faces_path, 'Homer'))) * train_percent)
+test_homer_dir = os.path.join(test_dir, 'homer')
+test_no_homer_dir = os.path.join(test_dir, 'no_homer')
 
-len(os.listdir(os.path.join(faces_path, 'Homer')))
-print(train_size)
+# Our defined training proportion will be 70%, validation will be 15% and test will be 15%:
+train_percent = 0.70
+validation_percent = 0.15
+test_percent = 1 - (train_percent - validation_percent)
 
-rand_homer_list = np.random.RandomState(0).choice(os.listdir(os.path.join(faces_path, 'Homer')), len(os.listdir(os.path.join(faces_path, 'Homer'))))
-rand_no_homer_list = np.random.RandomState(0).choice(os.listdir(os.path.join(faces_path, 'No_Homer')), len(os.listdir(os.path.join(faces_path, 'No_Homer'))))
+dataset_size_per_class = len(os.listdir(homer_dir))
 
+train_size = int(dataset_size_per_class * train_percent)
+val_size = int(dataset_size_per_class * validation_percent)
+test_size = int(dataset_size_per_class * test_percent)
+
+# We are just checking out if training proportion worked out well, to double check it, uncomment the following 2 lines: We can very well delete this later
+# print(f'Total number of homer face images: {len(os.listdir(os.path.join(faces_path, "Homer")))}')
+# print(f'Training dataset size: {train_size}')
+
+#We are randomizing the list of file names for each directory
+rand_homer_list = np.random.RandomState(0).choice(os.listdir(homer_dir), len(os.listdir(homer_dir)))
+rand_no_homer_list = np.random.RandomState(0).choice(os.listdir(no_homer_dir), len(os.listdir(no_homer_dir)))
+
+#Training and validation randomized lists for both directories (with and without homer)
 rand_homer_train_list = rand_homer_list[:train_size]
-rand_homer_val_list = rand_homer_list[train_size:]
+rand_homer_val_list = rand_homer_list[train_size:(train_size + val_size)]
+rand_homer_test_list = rand_homer_list[(train_size + val_size):]
 
 rand_no_homer_train_list = rand_no_homer_list[:train_size]
-rand_no_homer_val_list = rand_no_homer_list[train_size:]
-
-# print(f'Homer train list sample {rand_homer_train_list[:5]}')
-# print(f'Homer validation list sample {rand_homer_val_list[:5]}')
-# print(f'No Homer train list sample {rand_no_homer_train_list[:5]}')
-# print(f'No Homer validation list sample {rand_no_homer_val_list[:5]}')
-#
-# print(f'Homer train list size {len(rand_homer_train_list)}')
-# print(f'Homer validation list size {len(rand_homer_val_list)}')
-# print(f'No Homer train list size {len(rand_no_homer_train_list)}')
-# print(f'No Homer validation list size {len(rand_no_homer_val_list)}')
+rand_no_homer_val_list = rand_no_homer_list[train_size:(train_size + val_size)]
+rand_no_homer_test_list = rand_no_homer_list[(train_size + val_size):]
 
 
-src = os.path.join(faces_path, rand_homer_train_list[0])
-print(src)
+# Now we want to create the directory structure within the Pycharm project directory and copy the images there.
+directories = [train_homer_dir,
+               validation_homer_dir,
+               test_homer_dir,
+               train_no_homer_dir,
+               validation_no_homer_dir,
+               test_no_homer_dir]
 
-dst = train_homer_dir
-print(dst)
+# If the directories do not exist, create them:
+for directory in directories:
+    if not os.path.isdir(directory):
+        # print(directory)
+        os.makedirs(directory)
 
-dst = 'D:\Data Science/Projects/Homer_detector/train/homer'
-dst = r'C:\Users\RodrigoRoman\PycharmProjects\Homer_Face_Detector\Delete'
+# For each image in each randomized group, copy it into it's corresponding directory:
+for rand_homer_train_image, rand_homer_val_image, rand_homer_test_image, rand_no_homer_train_image, rand_no_homer_val_image, rand_no_homer_test_image in zip(rand_homer_train_list,
+                                                                                                                                                             rand_homer_val_list,
+                                                                                                                                                             rand_homer_test_list,
+                                                                                                                                                             rand_no_homer_train_list,
+                                                                                                                                                             rand_no_homer_val_list,
+                                                                                                                                                             rand_no_homer_test_list):
 
-os.mkdir(dst)
+    shutil.copy2(os.path.join(homer_dir, rand_homer_train_image), train_homer_dir)
+    shutil.copy2(os.path.join(homer_dir, rand_homer_val_image), validation_homer_dir)
+    shutil.copy2(os.path.join(homer_dir, rand_homer_test_image), test_homer_dir)
+    shutil.copy2(os.path.join(no_homer_dir, rand_no_homer_train_image), train_no_homer_dir)
+    shutil.copy2(os.path.join(no_homer_dir, rand_no_homer_val_image), validation_no_homer_dir)
+    shutil.copy2(os.path.join(no_homer_dir, rand_no_homer_test_image), test_no_homer_dir)
 
-shutil.copy2(src, dst)
+
 
 
 def print_hi(name):
